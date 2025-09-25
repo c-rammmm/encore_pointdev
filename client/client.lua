5770924db977a83a75f94954ccf715f600b7a0ce
c-rammmm <146345757+c-rammmm@users.noreply.github.com>
2025-09-25 12:10:16 -0400

    Update client.lua
    Supports vector4

--- client/client.lua
+++ client/client.lua
@@ -8,11 +8,11 @@ local isCollecting = false
 function startCollectionThread()
     points = {}
 
-    AddTextEntry('pointDevAlert', 'Press ~INPUT_PICKUP~ to save point.')
+    AddTextEntry('pointDevAlert', 'Press ~INPUT_PICKUP~ (E) for vector3 or ~INPUT_DETONATE~ (G) for vector4.')
 
-    Citizen.CreateThread(function()
+    CreateThread(function()
         while true do
-            Citizen.Wait(0)
+            Wait(0)
 
             if not isCollecting then
                 return
@@ -21,12 +21,13 @@ function startCollectionThread()
             BeginTextCommandDisplayHelp('pointDevAlert')
             EndTextCommandDisplayHelp(0, false, false, -1)
         
-            if IsControlJustReleased(0, 38) then
+            -- Save vector3 with E
+                if IsControlJustReleased(0, 38) then
                 local playerCoordinates = GetEntityCoords(PlayerPedId())
 
                 playerCoordinates = (playerCoordinates - vector3(0.0, 0.0, 1.0))
         
-                table.insert(points, playerCoordinates)
+                table.insert(points, {coords = playerCoordinates, heading = nil})
         
                 TriggerEvent('chat:addMessage', {
                     color     = { 255, 0, 0 },
@@ -36,6 +37,27 @@ function startCollectionThread()
                         mathRound(playerCoordinates.x, 2),
                         mathRound(playerCoordinates.y, 2),
                         mathRound(playerCoordinates.z, 2)
+                    )}
+                })
+            end
+                
+            -- Save vector4 with G
+            if IsControlJustReleased(0, 47) then
+                local playerCoordinates = GetEntityCoords(PlayerPedId())
+                playerCoordinates = (playerCoordinates - vector3(0.0, 0.0, 1.0))
+                local heading = GetEntityHeading(PlayerPedId())
+
+                table.insert(points, {coords = playerCoordinates, heading = heading})
+
+                TriggerEvent('chat:addMessage', {
+                    color     = { 0, 255, 0 },
+                    multiline = true,
+                    args      = {"PointDev", ("Saved Point #%s: vector4(%s, %s, %s, %s)"):format(
+                        #points,
+                        mathRound(playerCoordinates.x, 2),
+                        mathRound(playerCoordinates.y, 2),
+                        mathRound(playerCoordinates.z, 2),
+                        mathRound(heading, 2)
                     )}
                 })
             end
