422ca03162558d1a56cf07e51b99d0c8eac62ead
c-rammmm <146345757+c-rammmm@users.noreply.github.com>
2025-09-25 12:12:34 -0400

    Update server.lua
    Supports vector4

--- server/server.lua
+++ server/server.lua
@@ -20,11 +20,20 @@ AddEventHandler('encore_pointdev:points', function(points)
     local textString = ''
 
     for k,v in pairs(points) do
+            if v.heading then
+            textString = textString .. ("vector4(%s, %s, %s, %s),\n"):format(
+                mathRound(v.coords.x, 2),
+                mathRound(v.coords.y, 2),
+                mathRound(v.coords.z, 2),
+                mathRound(v.heading, 2)
+            )
+        else
         textString = textString .. ("vector3(%s, %s, %s),\n"):format(
-            mathRound(v.x, 2),
-            mathRound(v.y, 2),
-            mathRound(v.z, 2)
-        )
+                mathRound(v.coords.x, 2),
+                mathRound(v.coords.y, 2),
+                mathRound(v.coords.z, 2)
+            )
+        end
     end
 
     local time = os.time(os.date('*t'))
